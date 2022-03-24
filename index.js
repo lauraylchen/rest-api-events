bd7136af08b8b223c22598cc16ac4ac928b0a22e
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-03-24 14:55:32 -0400

    Testing to be able to display list of events in json

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const app = express();
+const PORT = 8080;
+
+// Testing events
+const events = [
+  {
+    id: 1,
+    name: 'event1',
+    description: 'This event is a test.',
+    start_date: '2022-03-25 08:11:51',
+    end_date: '2022-03-25 14:11:51'
+  },
+  {
+    id: 2,
+    name: 'event2',
+    description: 'This event is a test.',
+    start_date: '2022-03-25 08:11:51',
+    end_date: '2022-03-26 14:11:51'
+  }
+];
 
 // Display all the events
-app.get();
+app.get('/events', (req, res) => {
+  res.send(events)
+});
 
 // Create an event
-app.post();
+// app.post();
+
+app.listen(
+  PORT,
+  () => console.log(`It's alive on http://localhost:${PORT}`)
+);
