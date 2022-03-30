bbd92bed4598da1b31e1943ce3c8de1537535525
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-03-30 16:54:56 -0400

    Return events from DB

--- routes/events.js
+++ routes/events.js
@@ -1,41 +1,32 @@
 const express = require('express');
 const router = express.Router();
-
-// Testing events
-const events = [
-  {
-    id: 1,
-    name: 'event1',
-    description: 'This event is a test.',
-    start_date: '2022-03-25 08:11:51',
-    end_date: '2022-03-25 14:11:51'
-  },
-  {
-    id: 2,
-    name: 'event2',
-    description: 'This event is a test.',
-    start_date: '2022-03-25 08:11:51',
-    end_date: '2022-03-26 14:11:51'
-  }
-];
+const Event = require('../models/Event');
 
 // Display all the events
-router.get('/', (req, res) => {
-  res.send(events)
+router.get('/', async (req, res) => {
+  try {
+    const events = await Event.find();
+    res.json(events);
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
 // Create an event
-router.post('/', (req, res) => {
-  const event = {
-    id: events.length + 1,
+router.post('/', async (req, res) => {
+  const event = new Event ({
     name: req.body.name,
     description: req.body.description,
     start_date: req.body.start_date,
     end_date: req.body.end_date
-  };
+  });
 
-  events.push(event);
-  res.send(events);
+  try {
+    const savedEvent = await event.save();
+    res.json(savedEvent);
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
 module.exports = router;
